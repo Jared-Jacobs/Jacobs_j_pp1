5c5331b3bc5858ed6a54348a9bd5769722644672
Jared-Jacobs <jjacobs3@bellarmine.edu>
2019-09-12 18:09:43 -0400

    Added authlog and updated Application

--- Application.java
+++ Application.java
@@ -1,1 +1,22 @@
+import java.util.*;
+import java.util.regex.*;
+/**
+* Main Application that reads through the auth.log file and prints the file.
+* @author Jared Jacobs
+* @version 1.0
+* Programming Project One
+* CS322 - Compiler Construction
+* Spring 2019
+*/
+public class Application{
 
+
+	public static void main(String[] args){
+
+		HashMap<String, Integer> map = new Hashmap<>();
+		String authFile = "auth.log";
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+}
