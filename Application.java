30660917776798e5e33c84626612b3483bbc9e28
Jared-Jacobs <jjacobs3@bellarmine.edu>
2019-09-15 14:41:22 -0400

    Updated Application and Patterns can now grab Usernames aswell

--- Application.java
+++ Application.java
@@ -1,5 +1,6 @@
 import java.util.*;
-import java.util.regex.*;
+import java.io.*;
+
 /**
 * Main Application that reads through the auth.log file and prints the file.
 * @author Jared Jacobs
@@ -12,23 +13,44 @@ public class Application{
 
 
 	public static void main(String[] args){
-		String authFile = "auth.log");
-		String input= "";
 
-		try {
-			Scanner scan = new Scanner(authFile);
-			while(scan.hasNextLine()) {
-				input += scan.hasNextLine() + " ";
+		int totalLines = 0;
+
+		Patterns patterns = new Patterns();
+
+		try
+		{
+			Scanner scan = new Scanner(new File(args[0]));
+			while(scan.hasNextLine())
+			{
+				totalLines++;
+				patterns.Patterns(scan.nextLine());
+			}
+
 		}
-		catch (FileNotFoundException f) {
+		catch (FileNotFoundException f)
+		{
 			f.printStackTrace();
 		}
 
-		System.out.println(input);
-		Pattern pattern = Pattern.compile(("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
-		Matcher match = pattern.matcher(input);
-		while(match.find()) {
-			System.out.println("IP: " +match.group());
+		if(args[1].equals("1"))
+		{
+			patterns.getIpMap().forEach((k,v)->System.out.println("IP Address: " + k + "; : " + v));
 		}
 
+
+		if(args[1].equals("2"))
+		{
+			patterns.getUserMap().forEach((k,v)->System.out.println(k + ": " + v));
+		}
+		System.out.println(totalLines + " total lines were parsed.");
+		System.out.println("There are " + patterns.getIpMapSize() + " total Ip Addresses in the log.");
+		System.out.println("There are " + patterns.getUserMapSize() + " total Users in the log.");
+		//System.out.println(input);
+		//Matcher match = pattern.matcher(input);
+		//while(match.find()) {
+		//	System.out.println("IP: " +match.group());
+		//}
+
+	}
 }
--- Patterns.java
+++ Patterns.java
@@ -1,6 +1,6 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.Hashmap;
+
+import java.util.regex.*;
+import java.util.HashMap;
 /**
  * This classes uses REGEX to find IP addresses and usernames
  * and store them into 2 different hashmaps.
@@ -11,22 +11,69 @@ import java.util.Hashmap;
  * Spring 2019
  */
 
-public class PatternMatch
+public class Patterns
 {
 
-	//Declaring hashmaps for ip and usernames.
-	Hashmap<Integer, String> ipMap = new HashMap(Integer, String>();
-	Hashmap<Integer, String> userMap = new HashMap(Integer, String>();
+		HashMap<String, Integer> IpMap = new HashMap<>();
+		HashMap<String, Integer> UserMap = new HashMap<>();
 
-	public PatternMatch()
+	public Patterns()
 	{
-		ipMap = new HashMap<>();
-		userMap = new HashMap<>();
+		 IpMap = new HashMap<>();
+		 UserMap = new HashMap<>();
 	}
 
 	public HashMap<String, Integer> getIpMap()
 	{
-		return ipMap;
+		return IpMap;
 	}
 
+	public HashMap<String, Integer> getUserMap()
+	{
+		return UserMap;
+	}
 
+	public int getIpMapSize()
+	{
+		return IpMap.size();
+	}
+
+	public int getUserMapSize()
+	{
+		return UserMap.size();
+	}
+
+	public void Patterns(String str)
+	{
+		Matcher regexIp = Pattern.compile("\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b").matcher(str); 
+
+		Matcher regexUser = Pattern.compile("user\\s\\w+").matcher(str);
+
+	while(regexIp.find())
+	{
+		String ipPattern = regexIp.group();
+		if(IpMap.containsKey(ipPattern))
+		{
+			IpMap.put(ipPattern, IpMap.get(ipPattern) + 1);
+		}
+		else
+		{
+			IpMap.put(ipPattern,1);
+		}
+	}
+	while(regexUser.find())
+	{
+		String userPattern = regexUser.group();
+		String usernames = userPattern.substring(5);
+		if(UserMap.containsKey(userPattern))
+		{
+			UserMap.put(usernames, UserMap.get(usernames) + 1);
+
+		}else
+		{
+			UserMap.put(usernames, 1);
+		}
+
+	}
+	}
+}
